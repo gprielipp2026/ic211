085f3c64087b9216302b669515aa4691175d27a4
gprielipp2026 <m265112@usna.edu>
2024-01-26 10:43:12 -0500

    lab03

---
+++ lab03/Box.java
@@ -0,0 +1,37 @@
+import java.lang.Math;
+
+public class Box
+{
+  // constructor for Box taking a single point
+  public Box(Point p) 
+  {
+
+  }
+
+  // constructor for Box taking two initial points
+  public Box(Point a, Point b)
+  {
+
+  }
+
+  // expand the bounding box (if needed) to include point p
+  public void growBy(Point p)
+  {
+
+  }
+
+  // given point p in the bounding box, return associated
+  // point in the unit square (see notes); return null if
+  // p is not inside the bounding box.
+  public Point mapIntoUnitSquare(Point p)
+  {
+
+  }
+
+  // returns string representation like:
+  // 2.0 < x < 9.0, 3.0 < y < 7.0
+  public String toString()
+  {
+
+  }
+}
---
+++ lab03/Point.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Point
+{
+  
+  // initialize Point
+  public Point(double x, double y)
+  {
+
+  }
+
+  // reads an x and y value from the scanner
+  // and returns the associated point
+  public static Point read(Scanner sc)
+  {
+
+  }
+  
+  // returns a string that looks like:
+  // x y
+  public String toString()
+  {
+
+  }
+
+  // test the methods in Point class
+  public static void main(String[] args)
+  {
+
+  }
+}
