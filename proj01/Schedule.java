df489a1aabb422fdc7f2e37c3bcb4150669caee1
gprielipp2026 <m265112@usna.edu>
2024-02-02 15:10:27 -0500

    attempt2

--- proj01/Schedule.java
+++ proj01/Schedule.java
@@ -1,83 +1,9 @@
 /**
- * Stores a Queue of schedules and has a method for 
- * printint out the schedule
- * public methods:
- * Section()
- * void read(Scanner in)
- * boolean add(String className, int section)
- * void show()
- * void week()
  */
 
 import java.util.*;
 
 public class Schedule
 {
-  private Queue allCourses;
-  private Queue courses;
 
-  public Section()
-  {
-    allCourses = new Queue();
-    courses = new Queue();
-  }
-
-  /**
-   * Read from a file and fill out allCourses
-   */
-  public void read(Scanner in)
-  {
-    while(in.hasNext())
-    {
-      Section section = Section.read(in);
-      allCourses.enqueue(section);
-    }
-  }
-
-  /**
-   * Return the index of a given className in allClasses
-   * or -1 if it doesn't exist
-   */
-  private int locate(String className, int section)
-  {
-    int index = 0;
-    Section tmp;
-    while((tmp = allCourses.at(index)) != null)
-    {
-      if(tmp.equals(className, section))
-        return index;
-      index++;
-    }
-    return -1;
-  }
-
-  /**
-   * Add a section from allCourses to courses
-   * return true if able to add the course
-   * false if the course is unavailable to add to the 
-   * schedule
-   */
-  public boolean add(String className, int section)
-  {
-    int index = this.locate(className, section);
-    if(index == -1) 
-      return false;
-
-    courses.enqueue( allCourses.at(index) );
-    return true;
-  }
-
-  /**
-   * Print all of the sections in a row
-   */
-  public void show()
-  {
-    int index = 0;
-    Section tmp;
-    while((tmp = allCourses.at(index++)) != null)
-    {
-      System.out.println(tmp);  
-    }
-  }
-
-  }
+}
--- proj01/Section.java
+++ proj01/Section.java
@@ -1,83 +1,11 @@
 /**
  * Will store <Class> <Section> <Meeting Times> <Building Location>
  * Will be searchable only by Class
- * public methods:
- * static Section read(Scanner sc)
- * boolean equals(String className, int section)
- * String toString()
- */
+*/
 
 import java.util.*;
 
 public class Section
 {
-  private String className;
-  private int section;
-  private Week mtgTimes;
-  private String bldgLocation;
 
-  /**
-   * I don't want anyone to be able to create a 
-   * new Section. Only call the read method
-   * Make sure everything is initialized to a "zero"
-   */
-  private Section() 
-  {
-    className = null;
-    section = 0;
-    bldgLocation = null;
-    mtgTimes = new Week();
-  }
-
-
-  
-
-  /**
-   * Takes a Scanner and reads from input in the format:
-   * <Class> <Section> <Meeting Times> <Building Location>
-   * Ex:
-   * IC211  2004  MW2,R12 MI302
-   * Note: Fields to be read in are tab separated (trusting
-   * the Scanner to be passed in correctly)
-   */
-  public static Section read(Scanner in)
-  {
-    Section output = new Section();
-    
-    output.className = in.next();
-    output.section = in.nextInt();
-    String mtgTimesStr = in.next();
-    output.bldgLocation = in.next();
-
-    mtgTimes.fillMtgTimesByDay(mtgTimesStr);
-
-    return output;
-  }
-
-  /**
-   * Determine if this className is equal to 
-   * another className and section
-   */
-  public boolean equals(String className, int section)
-  {
-    return className.equals(this.className) && this.section == section;
-  }
-
-  /**
-   * Output in the form
-   * <Class> <Section> <Meeting Times> <Building Location>
-   */
-  public String toString()
-  {
-    return className + "\t" + section + "\t" + mtgTimes + "\t" + bldgLocation;
-  }
-
-   /**
-   * Test my methods
-   */
-  public static void main(String[] args)
-  {
-    String test = "MWF1256";
-    
-  }
 }
