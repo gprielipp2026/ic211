e437c888cb13d1dcc828d5a7f0263167456d0632
gprielipp2026 <m265112@usna.edu>
2024-03-01 09:56:33 -0500

    lab08

--- lab07/ThingE.java
+++ lab07/ThingE.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class ThingE extends ThingC
 {
   private int prow, pcol;
-  private int speed = 6;
+  private int speed = 3;
   public ThingE(int row, int col, char color)
   {
     super(row, col, color);
@@ -11,21 +11,21 @@ public class ThingE extends ThingC
     pcol = col;
   }
 
-  public void step()
-  {
-    for(int i = 0; i < speed; i++)
-    {
-      super.step();
-      if(i < speed-1) 
-        System.out.println(this);
-    }
-  }
-
-  // public String toString()
+  // public void step()
   // {
-  //   String ret = prow + " " + pcol + " " + color + "\n" + super.toString();
-  //   prow = row;
-  //   pcol = col;
-  //   return ret;
+  //   for(int i = 0; i < speed; i++)
+  //   {
+  //     super.step();
+  //     if(i < speed-1) 
+  //       System.out.println(this);
+  //   }
   // }
+
+  public String toString()
+  {
+    String ret = prow + " " + pcol + " " + color + "\n" + super.toString();
+    prow = row;
+    pcol = col;
+    return ret;
+  }
 }
