7519339070688fbf740a5b2641f58b37b8ce782f
gprielipp2026 <m265112@usna.edu>
2024-03-25 19:15:22 -0400

    hw17

--- hw17/HW1.java
+++ hw17/HW1.java
@@ -5,14 +5,22 @@ public class HW1
 {
   public static void main(String[] args)
   {
-    Reader r = new InputStreamReader(System.in);
+    LineNumberReader r = null;
+    if(args.length > 0)
+    {
+      try { r = new LineNumberReader(new FileReader(args[0])); }
+      catch (Exception e) { e.printStackTrace(); }
+    }
+    else
+      r = new LineNumberReader(new InputStreamReader(System.in));
+
     Scanner sc = new Scanner(r);
     try {
       System.out.println(Mystery.compute(sc));
     }
     catch(Exception e)
     {
-      System.out.println("Error " + e.getMessage());
+      System.out.println("Error " + e.getMessage() + " at line " + r.getLineNumber());
     }
   }
 }
